62494de483746968dbdc58d5f4623d27db63dd37
Danny Leshem <dleshem@gmail.com>
2017-12-16 01:12:50 +0200

    DateTimeWindow.toString

--- wix-restaurants-availability-api/src/main/java/com/wix/restaurants/availability/DateTimeWindow.java
+++ wix-restaurants-availability-api/src/main/java/com/wix/restaurants/availability/DateTimeWindow.java
@@ -45,7 +45,7 @@ public class DateTimeWindow implements Serializable, Cloneable {
     public DateTimeWindow() {}
     
     @Override
-	protected Object clone() {
+	protected DateTimeWindow clone() {
     	return new DateTimeWindow(
     			((start != null) ? (Date) start.clone() : null),
     			((end != null) ? (Date) end.clone() : null),
@@ -61,10 +61,45 @@ public class DateTimeWindow implements Serializable, Cloneable {
 
 		final List<DateTimeWindow> cloned = new ArrayList<>(windows.size());
 		for (DateTimeWindow window : windows) {
-			cloned.add((window != null) ? (DateTimeWindow) window.clone() : null);
+			cloned.add((window != null) ? window.clone() : null);
 		}
 		return cloned;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		DateTimeWindow that = (DateTimeWindow) o;
+
+		if (start != null ? !start.equals(that.start) : that.start != null) return false;
+		if (end != null ? !end.equals(that.end) : that.end != null) return false;
+		if (available != null ? !available.equals(that.available) : that.available != null) return false;
+		if (reason != null ? !reason.equals(that.reason) : that.reason != null) return false;
+		return comment != null ? comment.equals(that.comment) : that.comment == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = start != null ? start.hashCode() : 0;
+		result = 31 * result + (end != null ? end.hashCode() : 0);
+		result = 31 * result + (available != null ? available.hashCode() : 0);
+		result = 31 * result + (reason != null ? reason.hashCode() : 0);
+		result = 31 * result + (comment != null ? comment.hashCode() : 0);
+		return result;
+	}
+
+    @Override
+    public String toString() {
+        return "DateTimeWindow{" +
+                "start=" + start +
+                ", end=" + end +
+                ", available=" + available +
+                ", reason='" + reason + '\'' +
+                ", comment=" + comment +
+                '}';
+    }
 
     public Calendar start(TimeZone tz) {
         return ((start != null) ? start.calendar(tz) : null);
@@ -92,53 +127,4 @@ public class DateTimeWindow implements Serializable, Cloneable {
     /** Additional reason information (localized free-text). */
     @JsonInclude(Include.NON_DEFAULT)
     public Map<String, String> comment = new LinkedHashMap<>();
-    
-    @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((available == null) ? 0 : available.hashCode());
-		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
-		result = prime * result + ((end == null) ? 0 : end.hashCode());
-		result = prime * result + ((reason == null) ? 0 : reason.hashCode());
-		result = prime * result + ((start == null) ? 0 : start.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DateTimeWindow other = (DateTimeWindow) obj;
-		if (available == null) {
-			if (other.available != null)
-				return false;
-		} else if (!available.equals(other.available))
-			return false;
-		if (comment == null) {
-			if (other.comment != null)
-				return false;
-		} else if (!comment.equals(other.comment))
-			return false;
-		if (end == null) {
-			if (other.end != null)
-				return false;
-		} else if (!end.equals(other.end))
-			return false;
-		if (reason == null) {
-			if (other.reason != null)
-				return false;
-		} else if (!reason.equals(other.reason))
-			return false;
-		if (start == null) {
-			if (other.start != null)
-				return false;
-		} else if (!start.equals(other.start))
-			return false;
-		return true;
-	}
 }
